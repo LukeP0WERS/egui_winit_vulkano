88338ab0986f2319b6e9d467058a8964ace88af8
LukeP0WERS <lukepowers.w@gmail.com>
2026-05-11 15:23:26 -0400

    make `EguiSystemError` public

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@
 mod egui_system;
 mod image_utils;
 
-pub use egui_system::{EguiSystem, EguiSystemConfig, EguiTexture, RenderEguiWorld};
+pub use egui_system::{EguiSystem, EguiSystemConfig, EguiSystemError, EguiTexture, RenderEguiWorld};
 #[cfg(feature = "image")]
 pub use image_utils::immutable_texture_from_file;
 pub use image_utils::{immutable_texture_from_bytes, AlphaMode};
